c46664d98a93d8241cc448edb125e194d11f4f64
Hanae <hanaebennani86@gmail.com>
2022-03-08 15:12:04 +0100

    Repository T introuvable

--- Repositories/Repository.cs
+++ Repositories/Repository.cs
@@ -14,13 +14,60 @@ namespace Hotel.Repositories
 			Context = context;
         }
 
-      //  public async Task Add(T obj)
-       // {
-         //   try
-           // {
-               // await Context.
-           // }
-
-       // }
-	}
+        public async Task Add(T obj)
+       {
+            try
+           {
+                await Context.AddAsync<T>(obj);
+                await Context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+            }
+        }
+        public async Task<T> GetById(int id)
+        {
+            try
+            {
+                return await Context.FindAsync(id);
+            }
+            catch(Exception)
+            {
+                return new T();
+            }
+        }
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            try
+            {
+                return await Context.ToListAsyn();
+            }
+            catch (Exception)
+            {
+                return new List<T>();
+            }
+        }
+        public async Task Delete(T obj)
+        {
+            try
+            {
+                Context.Remove(T);
+                await Context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+            }
+        }
+        public async Task Update(T obj)
+        {
+            try
+            {
+                Context.Update(T);
+                await Context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
 }
