9e73101344752959a87268233dddd4feca624bf1
Kevin SAW <sawadogokevin98@gmail.com>
2022-04-15 00:38:38 +0000

    Modification de la classe Personne

--- Models/Personne.cs
+++ Models/Personne.cs
@@ -10,13 +10,25 @@ namespace Hotel.Models
 	{
 		[Key]
 		public string IdPersonne { get; set; }
+
+		public string IdFirebasePersonne { get; set; }
+
 		public string Nom { get; set; }
+		
 		public string Prenom { get; set; }
+		
+		public string Adresse { get; set; }
+
 		public string Email { get; set; }
+		
 		public string Telephone { get; set; }
+		
 		public string Mot_de_passe { get; set; }
-		public bool IsAdult { get; set; }
+		
+		public int Age { get; set; }
+		
 		public string Sexe { get; set; }
-		public string Privelege { get; set; }
+		
+		public string Privilege { get; set; }
 	}
 }
